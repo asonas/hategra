bc0220e10fbec6d303ace1d5bf62ab4f9569aebf
asonas <hzw1258@gmail.com>
2013-05-06 23:28:23 +0900

    Fix a bug

--- lib/hategra.rb
+++ lib/hategra.rb
@@ -2,9 +2,12 @@ require "hategra/version"
 require "erb"
 
 module Hategra
-  urls = ARGV[0].delete("[]").split(":image=")
+  root = File.expand_path '../..', __FILE__
+  template_path = File.join root, "lib/template.erb"
 
-  template = ERB.new(File.read("./lib/template.erb"))
+  template = ERB.new(File.read(template_path))
+
+  urls = ARGV[0].delete("[]").split(":image=")
 
   system("echo '#{template.result(binding)}' | pbcopy")
 end
