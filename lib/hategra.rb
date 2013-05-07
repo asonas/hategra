4855cccf2dc9228d5b121a3f6f6beed9e9793d8f
asonas <hzw1258@gmail.com>
2013-05-08 00:34:35 +0900

    Error was set to come out when not supply the argument

--- lib/hategra.rb
+++ lib/hategra.rb
@@ -2,6 +2,12 @@ require "hategra/version"
 require "erb"
 
 module Hategra
+  unless ARGV[0]
+    puts "Please give as arguments Hatena-Notation"
+    puts "Ex. [http://instagram.com/p/Y-Q9qvNBbh/:image=http://distilleryimage5.s3.amazonaws.com/781e7932b65511e29a5822000a9f15d4_7.jpg]"
+    exit
+  end
+
   root = File.expand_path '../..', __FILE__
   template_path = File.join root, "lib/template.erb"
 
@@ -10,4 +16,5 @@ module Hategra
   urls = ARGV[0].delete("[]").split(":image=")
 
   system("echo '#{template.result(binding)}' | pbcopy")
+  puts "Copied!"
 end
